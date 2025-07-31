ff589e55dcea11fde453739d3ff2d7dd45af0037
tranmanhthe <tranmanhthe@cityascom.vn>
2025-07-31 13:04:46 +0700

    up

---
+++ Customers.API/Entities/Customer.cs
@@ -0,0 +1,18 @@
+﻿using Contracts.Domains;
+using System.ComponentModel.DataAnnotations;
+
+namespace Customers.API.Entities
+{
+    public class Customer: EntityBase<int>
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [Required, EmailAddress, MaxLength(100)]
+        public string Email { get; set; }
+    }
+}
---
+++ Customers.API/Persistence/CustomerContextSeed.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Customers.API.Persistence
+{
+    public static class CustomerContextSeed
+    {
+        public static IHost SeedCustomerData(this IHost host)
+        {
+            {
+                using var scope = host.Services.CreateScope();
+                var customerContext= scope.ServiceProvider.GetRequiredService<CustomerContext>();
+                customerContext.Database.MigrateAsync().GetAwaiter().GetResult();
+                CreateCustomer(customerContext, "customer1", "customer1", "customer", "customer1@local.com").GetAwaiter().GetResult();
+                CreateCustomer(customerContext, "customer2", "customer2", "customer", "customer2@local.com").GetAwaiter().GetResult();
+                return host;
+            }
+        }
+        private static async Task CreateCustomer(CustomerContext customerContext, string userName, string firstName, string lastName, string email)
+        {
+            if (!await customerContext.Customers.AnyAsync(c => c.UserName == userName))
+            {
+                var customer = new Entities.Customer
+                {
+                    UserName = userName,
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Email = email
+                };
+                await customerContext.Customers.AddAsync(customer);
+                await customerContext.SaveChangesAsync();
+            }
+        }
+    }
+}
